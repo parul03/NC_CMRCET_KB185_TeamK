438da91dc586069f977f59d9d70f05432d4595bd
Shashwat <shashwatpaul@gmail.com>
2020-08-01 12:27:59 +0530

    Added basic models

---
+++ Web/SIH Dashboard/code/Models.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIH_Dashboard
+{
+    public static class GlobalUser
+    {
+        public static string Name { get; set; }
+        public static string UID { get; set; }
+        public static bool IsVerified { get; set; }
+        public static string Email { get; set; }
+    }
+    public class UserData
+    {
+        public string Name { get; set; }
+        public long Phone { get; set; }
+        public Roles Role { get; set; }
+        public string DOB { get; set; }
+        public bool Male { get; set; }
+        public string Email { get; set; }
+        public bool RoleVerified { get; set; }
+    }
+
+    public enum Roles
+    {
+        Admin, VisitingOfficial, Teacher, Student
+    }
+
+    public class Schools
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class Feedbacks
+    {
+        public string Date { get; set; }
+        public UserData User { get; set; }
+        public Schools School { get; set; }
+        public string Feedback { get; set; }
+    }
+}
